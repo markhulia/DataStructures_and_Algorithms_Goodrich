dd648c5612e22773704b869b9daed16510776c8b
markhulia <markhulia@Georges-MacBook-Pro.local>
2016-05-29 20:09:38 +0200

    updated C1

--- chapter1.py
+++ chapter1.py
@@ -1,323 +1,221 @@
+'''Write a short Python function, is multiple(n, m), that takes two
+integer values and returns True if n is a multiple of m, that is,
+n = mi for some integer i, and False otherwise.'''
 
-import struct
-#R-1.2
-'''Write a short Python function, is even(k), that takes an integer value and
-returns True if k is even, and False otherwise. However, your function
-cannot use the multiplication, modulo, or division operators.'''
-import re
+# R-1.1
 import string
-from collections import Counter
-from random import choice, randrange, randint
-import random
+from random import randint, randrange
+
+from math import factorial
 
 
-def isEven(m):
-    if (-1) ** m == 1:
-        print((-1)**m)
+def is_multiple(N, M):
+    if M%N == 0:
         return True
     return False
 
 
-#R-1.3
-'''Write a short Python function, minmax(data), that takes a sequence of
-one or more numbers, and returns the smallest and largest numbers, in the
-form of a tuple of length two. Do not use the built-in functions min or
-max in implementing your solution.'''
+# R-1.2
+
+
+# R-1.3
+'''Write a short Python function, minmax(data), that takes a sequence
+of one or more numbers, and returns the smallest and largest numbers,
+in the form of a tuple of length two. Do not use the built-in functions
+ min or max in implementing your solution.'''
 def minmax(data):
-    min = max = badVal = data[0]
-    try:
-        for i in range(len(data)):
-            if data[i] <= min:
-                min = data[i]
-            elif data[i]>=max:
-                max = data[i]
-            elif not isinstance(data[i] ,int):
-                pass
-    except:
-        print('bad values found at index:', i)
-    return min, max
+    data.sort()
+    return (data[0],data[-1])
 
 
-#R-1.4
-'''Write a short Python function that takes a positive integer n and returns
-the sum of the squares of all the positive integers smaller than n.'''
-def sqrsums(n):
-    if n>0:
-        sum = 0
-        for i in range(n+1):
-            sum += i**2
-        return sum
+# R-1.4
+'''Write a short Python function that takes a positive integer n and
+returns the sum of the squares of all the positive integers smaller
+ than n.'''
+def square_sums(N):
+    sum=0
+    for i in range(N):
+        sum+=i*i
+    return sum
+
+
+# R-1.5
+'''Give a single command that computes the sum from Exercise R-1.4,
+ relying on Python’s comprehension syntax and the built-in
+ sum function.'''
+def square_sums_comprehension(N):
+    return sum([i*i for i in range(N)])
+
+
+# R-1.6
+'''Write a short Python function that takes a positive integer n
+ and returns the sum of the squares of all the odd positive
+ integers smaller than n.'''
+def odd_squares(N):
+    return sum([i*i for i in range(N) if i % 2 != 0])
+
+
+# C-1.14
+'''Write a short Python function that takes a sequence of integer
+ values and determines if there is a distinct pair of numbers in
+ the sequence whose product is odd.'''
+def odd_products(L):
+    lst = []
+    for i in range(len(L)):
+        if (L[i] % 2 != 0) and L[i] not in lst:
+            lst.append(L[i])
+    if len(lst)>2:
+        return L[0], L[1] #just return first two elements
     return False
 
 
-
-#R-1.5
-'''Give a single command that computes the sum from Exercise R-1.4, relying
-on Python’s comprehension syntax and the built-in sum function.'''
-def sqrsums2(n):
-    return sum([i**2 for i in range(n+1)])
-
-#R-1.6, R-1.7
-'''Write a short Python function that takes a positive integer n and returns
-the sum of the squares of all the odd positive integers smaller than n.'''
-def sqrOddsums(n):
-    return sum([i**2 for i in range(n+1) if i%2!=0])
-
-#R-1.11
-'''Demonstrate how to use Python’s list comprehension syntax to produce
-the list [1, 2, 4, 8, 16, 32, 64, 128, 256].'''
-# print([2**n for n in range(9)])
-
-
-#R-1.12
-''' Python’s random module includes a function choice(data) that returns a
-random element from a non-empty sequence. The random module includes
-a more basic function randrange, with parameterization similar to
-the built-in range function, that return a random choice from the given
-range. Using only the randrange function, implement your own version
-of the choice function.'''
-# from random import randrange
-# print(randrange(10))
-
-
 # C-1.15
-def ints():
-    l = []
-    n = input('Enter a number: ("Enter" to quit): ')
-    while n!='':
-        try:
-            if isinstance(int(n), int):
-                l.append(int(n))
-            n = input('Enter a number: ("Enter" to quit): ')
-        except:
-            print('not a number')
-            break
-
-    print(l)
-# C-1.15
-'''Write a short Python function that takes a sequence of integer values and
-determines if there is a distinct pair of numbers in the sequence whose
-product is odd.'''
-def odds(l):
-    for i in range(len(l)):
-        for n in range(i+1, len(l)):
-            if ((l[i]*l[n])%2!=0)\
-                    and (l[i]!=l[n]):
-                yield l[i], l[n]
-
-# for i in odds([1,2,3,3,4,5,6]):
-#     print(i)
+'''Write a Python function that takes a sequence of numbers and
+determines if all the numbers are different from each other
+ (that is, they are distinct).'''
+def distinct_numbers(L):
+    temp = []
+    for i in range(len(L)):
+        if L[i] not in temp:
+            temp.append(L[i])
+        else:
+            return False
+    if len(L)!=len(temp):
+        return False
+    return True
 
 
 # C-1.18
 '''Demonstrate how to use Python’s list comprehension syntax to produce
 the list [0, 2, 6, 12, 20, 30, 42, 56, 72, 90].'''
-# print([(x*(x-1)) for x in range(1,11)])
+# [i*(i-1) for i in range(1,10)]
+
 
 # C-1.19
-''' Demonstrate how to use Python’s list comprehension syntax to produce
-the list [ a , b , c , ..., z ], but without having to type all 26 such
-characters literally. '''
-# print([chr(x) for x in range(97, 123)])
+'''Demonstrate how to use Python’s list comprehension syntax to produce
+thelist[ a , b , c ,..., z ],butwithouthavingtotypeall26such characters
+literally.'''
+# [chr(i) for i in range(97, 123)]
 
 
 # C-1.20
-'''Python’s random module includes a function shuffle(data) that accepts a
-list of elements and randomly reorders the elements so that each possible
-order occurs with equal probability. The random module includes a
-more basic function randint(a, b) that returns a uniformly random integer
-from a to b (including both endpoints). Using only the randint function,
-implement your own version of the shuffle function.'''
-def shuffle(l):
-    for i in range(len(l)):
-        r = randint(i, len(l)-1)
-        l[i], l[r] = l[r], l[i]
-    print('\n',l)
+'''Python’s random module includes a function shuffle(data) that accepts
+a list of elements and randomly reorders the elements so that each
+possible order occurs with equal probability. The random module includes
+a more basic function randint(a, b) that returns a uniformly random
+integer from a to b (including both endpoints). Using only the randint
+function, implement your own version of the shuffle function.'''
+def _shuffle(L):
+    # shuffled = [None] * len(L)
+    # for i in range(len(shuffled)):
+    #     shuffled[i] = L.pop(randint(0, (len(L)-1)))
+    # return shuffled
+    for i in range(len(L)):
+        rand = L[randint(i, (len(L)-1))]
+        L[i],  L[rand]= L[rand], L[i]
+    return L
 
 
 # C-1.21
-'''Write a Python program that repeatedly reads lines from standard input
-until an EOFError is raised, and then outputs those lines in reverse order
-(a user can indicate end of input by typing ctrl-D).'''
-def reader():
-    l = []
-    try:
-        while EOFError:
-             l.append(input('enter line: '))
-
-    except EOFError:
-        for i in range(len(l)):
-            print(l[((len(l)-1)-i)])
-
-
-
-# C-1.22
-'''Write a short Python program that takes two arrays a and b of length n
-storing int values, and returns the dot product of a and b. That is, it returns
-an array c of length n such that c[i] = a[i] · b[i], for i = 0, . . . ,n−1.'''
-def dot_product(l1, l2):
-    # assuming both lists are of an equal length
-    dot = []
-    for i in range(len(l1)):
-        dot.append(l1[i]*l2[i])
-    print(dot)
-
-
-# C-1.23
-'''Give an example of a Python code fragment that attempts to write an element
-to a list based on an index that may be out of bounds. If that index
-is out of bounds, the program should catch the exception that results, and
-print the following error message:'''
-def overflow(l):
-    try:
-        l[len(l)] = 'new element'
-    except (IndexError):
-        print('“Don’t try buffer overflow attacks in Python!”')
-
-
-# C-1.24
-'''Write a short Python function that counts the number of vowels in a given
-character string.'''
-def vowels(s):
-    count=0
-    for i in s:
-        if i in 'aeiou':
-            count+=1
-    print(count)
-
-# C-1.25
-'''Write a short Python function that takes a string s, representing a sentence,
-and returns a copy of the string with all punctuation removed. For example,
-if given the string "Let s try, Mike.", this function would return
-"Lets try Mike".'''
-def punctuation(s):
-    return re.sub('[%s]' % re.escape(string.punctuation), '', s)
-
-
-# C-1.26
-'''Write a short program that takes as input three integers, a, b, and c, from
-the console and determines if they can be used in a correct arithmetic
-formula (in the given order), like “a+b = c,” “a = b−c,” or “a ∗ b = c.”'''
-def formula():
-    try:
-        a, b, c = eval(input('Enter three numbers separated by coma: '))
-    except:
-        print('srsly?')
-        formula()
-    if c == a+b or a == b-c or c==a*b:
-        return True
-    return False
+'''Remove punctuation for the string'''
+def remove_punctuation(S):
+    exclude = set(string.punctuation)
+    return ''.join(ch for ch in S if ch not in exclude)
 
 
 # C-1.27
-'''In Section 1.8, we provided three different implementations of a generator
-that computes factors of a given integer. The third of those implementations,
-from page 41, was the most efficient, but we noted that it did not
-yield the factors in increasing order. Modify the generator so that it reports
-factors in increasing order, while maintaining its general performance advantages.'''
+'''In Section 1.8, we provided three different implementations of a
+generator that computes factors of a given integer. The third of those
+implementations, from page 41, was the most efficient, but we noted
+that it did not yield the factors in increasing order. Modify the
+generator so that it reports factors in increasing order,
+while maintaining its general performance advantages.'''
+
 def factors(n):
-    k = 1
-    bigs =[]
+    k=1
+    c = -1
+    bigs = []
     while k*k<n:
-        if n % k == 0:
+        if n%k==0:
             yield k
-            bigs.append(n // k)
-        k += 1
+            bigs.append(n//k)
+        k+=1
     if k*k == n:
         yield k
-    bigs.reverse()
-    for n in bigs:
-        yield n
-
-# for i in factors(100):
-#     print(i)
-
+    for i in range(len(bigs)):
+        yield bigs[c]
+        c -= 1
 
 
 # P-1.29
 '''Write a Python program that outputs all possible strings formed by using
-the characters c , a , t , d , o , and g exactly once.'''
-def all_strings(str, iteration=0):
-    if iteration == len(str):
-        print(''.join(str))
+the characters c , a , t , d , o ,and g exactly once.'''
+def string_formatinon(S, iteration = 0):
+    if iteration==len(S):
+        print(''.join(S))
 
-    for i in range(iteration, len(str)):
-        str_copy = [char for char in str]
-        str_copy[i], str_copy[iteration] = str_copy[iteration], str_copy[i]
-        all_strings(str_copy, iteration+1)
-
-
-
+    for i in range(iteration, len(S)):
+        S_copy = [ch for ch in S]
+        S_copy[i], S_copy[iteration] = S_copy[iteration], S_copy[i]
+        string_formatinon(S_copy, iteration+1)
 
 
 # P-1.30
-'''Write a Python program that can take a positive integer greater than 2 as
-input and write out the number of times one must repeatedly divide this
-number by 2 before getting a value less than 2.'''
+'''Write a Python program that can take a positive integer greater
+than 2 as input and write out the number of times one must
+repeatedly divide this number by 2 before getting a value less
+than 2.'''
+def divide_by2(N):
+    step = 0
+    while N>1:
+        N /= 2
+        step += 1
+    return step
 
-def inStr():
-    n = -1
-    while n<2:
-        try:
-            n = int(input('Enter a positive integer greater than 2: '))
-        except:
-            pass
-    print(positive(n))
-
-
-def positive(n, step=0):
-    if n < 2:
-        # print(step)
-        return step
-    return positive(n//2, step+1)
-
-# inStr()
 
 # P-1.32
-'''Write a Python program that can simulate a simple calculator, using the
-console as the exclusive input and output device. That is, each input to the
-calculator, be it a number, like 12.34 or 1034, or an operator, like + or =,
-can be done on a separate line. After each such input, you should output
-to the Python console what would be displayed on your calculator.'''
-def simplecalc():
-    # simple calculator assumes every input on a new line
-    l = ''
+'''Write a Python program that can simulate a simple calculator,
+using the console as the exclusive input and output device.
+That is, each input to the calculator, be it a number,
+like 12.34 or 1034, or an operator, like + or =, can be
+done on a separate line. After each such input, you should
+output to the Python console what would be displayed on your
+calculator.'''
+def calculator():
+    screen = ''
     symbol = input()
     while symbol!='=':
-        if symbol not in '=*/.+-()':
-            if not isinstance(int(symbol), int):
-                print('wrong symbol')
-        l+=symbol
+        if symbol not in '+-*/()':
+            try:
+                isinstance(int(symbol), int)
+            except:
+                return 'Wrong Symbol'
+        screen+=symbol
         symbol=input()
-    print(eval(l))
+    return eval(screen)
 
 
 # P-1.34
-'''A common punishment for school children is to write out a sentence multiple
-times. Write a Python stand-alone program that will write out the
-following sentence one hundred times: “I will never spam my friends
-again.” Your program should number each of the sentences and it should
-make eight different random-looking typos.'''
-#return random indices for sentences which will be mistyped
-def charIndex(sentence):
-    return randrange(0, len(sentence))
+'''A common punishment for school children is to write out a
+sentence multiple times. Write a Python stand-alone program
+that will write out the following sentence one hundred
+times: “I will never spam my friends again.” Your program
+should number each of the sentences and it should make eight
+different random-looking typos.'''
+def typos(sentence):
+    random_sentences = [randint(1,100) for _ in range(8)]
+    random_sentences.sort()
+    # print(random_sentences)
+    for i in range(100):
+        if random_sentences != []:
+            if i == random_sentences[0]:
+                print('typo>>>',sentence.replace(sentence[randint(0, len(sentence))],
+                                       chr(randint(97,122)),1))
+                random_sentences.pop(0)
 
-#return random char indices in the sentence which will be mistyped
-def sentenceIndex(rng):
-    return random.sample(range(0, rng),8) #returns UNIQUE random numbers in the list
+        print((i+1),(sentence))
 
-def typo(sentence):
-    sentenceIndices = sorted(sentenceIndex(100))
-    sl = 0
-    for n in range(0, 100):
-        if sl<=7 and sentenceIndices[sl]==n:
-            print(n+1,sentence.replace(sentence[charIndex(sentence)],
-                                   chr(randrange(97, 123)),1)) #replace char with random char
-            sl+=1
-        else:
-            print(n+1, sentence)
 
-# typo('I will never spam my friends again.')
 
 # P-1.35
 '''The birthday paradox says that the probability that two people in a room
@@ -326,38 +224,32 @@ people in the room, is more than 23. This property is not really a paradox,
 but many people find it surprising. Design a Python program that can test
 this paradox by a series of experiments on randomly generated birthdays,
 which test this paradox for n = 5,10,15,20, . . . ,100.'''
-
-#generate random birthday dates (month, day)
-def randomDate():
-    month = randrange(1,13)
-    if month in [1,3,5,7,8,10,12]:
-        day = randrange(1,32)
-    elif month in [4,6,9,11]:
-        day = randrange(1,31)
-    else:
-        day = randrange(1,29)
-    return month, day
+def birthday_paradox():
+    # generate list of lists with random birthday dates
+    birthdays = [[randint(1,365) for i in range(n)]for n in range(5,101,5)]
+    # dictionary for results with key=number of experiments and val=number of matched bdays
+    results = {i:0 for i in range(5,101,5)}
+    for i in birthdays:
+        statistics = stats(i)
+        results[len(i)]=[statistics, + statistics/len(i)]
+    return results
 
 
-def bdparadox(n, match = 0):
-    for i in range(1,n):
-        bd1 = randomDate()
-        for b in range (2, n):
-            bd2 = randomDate()
-            if bd1 == bd2:
-                match+=1
-    return n, match, match/n     #return number of trials, duplicats found
-                                 # and probability for current trial
-
-def stats():
-    l=[]
-    for n in range(5, 101, 5):
-        l.append(bdparadox(n))
-    printStats(l)
+def stats(L):
+    bdays = []
+    match = 0
+    for i in L:
+        if i in bdays:
+            match+=1
+        bdays.append(i)
+    return match
 
 
-def printStats(tpl):
-    print('Trials:\tMatches:\tProbability')
-    for i in tpl:
-        print(' ',i[0],'    ',i[1],'        ',i[2])
-stats()
+
+
+
+
+
+
+
+
--- chapter2.py
+++ chapter2.py
@@ -1,79 +1,121 @@
+#
+# class CreditCard:
+#   """A consumer credit card."""
+#
+#   def __init__(self, customer, bank, acnt, limit):
+#     """Create a new credit card instance.
+#
+#     The initial balance is zero.
+#
+#     customer  the name of the customer (e.g., 'John Bowman')
+#     bank      the name of the bank (e.g., 'California Savings')
+#     acnt      the acount identifier (e.g., '5391 0375 9387 5309')
+#     limit     credit limit (measured in dollars)
+#     """
+#     self._customer = customer
+#     self._bank = bank
+#     self._account = acnt
+#     self._limit = limit
+#     self._balance = 0
+#
+#   def get_customer(self):
+#     """Return name of the customer."""
+#     return self._customer
+#
+#   def get_bank(self):
+#     """Return the bank's name."""
+#     return self._bank
+#
+#   def get_account(self):
+#     """Return the card identifying number (typically stored as a string)."""
+#     return self._account
+#
+#   def get_limit(self):
+#     """Return current credit limit."""
+#     return self._limit
+#
+#   def get_balance(self):
+#     """Return current balance."""
+#     return self._balance
+#
+#   def charge(self, price):
+#     """Charge given price to the card, assuming sufficient credit limit.
+#
+#     Return True if charge was processed; False if charge was denied.
+#     """
+#     if price + self._balance > self._limit:  # if charge would exceed limit,
+#       return False                           # cannot accept charge
+#     else:
+#       self._balance += price
+#       return True
+#
+#   def make_payment(self, amount):
+#     """Process customer payment that reduces balance."""
+#     self._balance -= amount
+#
+# if __name__ == '__main__':
+#   wallet = []
+#   wallet.append(CreditCard('jorius', 'California Savings',
+#                            '5391 0375 9387 5309', 2500) )
+#   wallet.append(CreditCard('John Bowman', 'California Federal',
+#                            '3485 0399 3395 1954', 3500) )
+#   wallet.append(CreditCard('pumpkin', 'California Finance',
+#                            '5391 0375 9387 5309', 5000) )
+#
+#   for val in range(1, 20):
+#     wallet[0].charge(val)
+#     wallet[1].charge(2*val)
+#     wallet[2].charge(3*val)
+#
+#   for c in range(0,3):
+#     print('Customer =', wallet[c].get_customer())
+#     print('Bank =', wallet[c].get_bank())
+#     print('Account =', wallet[c].get_account())
+#     print('Limit =', wallet[c].get_limit())
+#     print('Balance =', wallet[c].get_balance())
+#     while wallet[c].get_balance() > 100:
+#       wallet[c].make_payment(100)
+#       print('New balance =', wallet[c].get_balance())
+#     print()
 
-class CreditCard:
-  """A consumer credit card."""
 
-  def __init__(self, customer, bank, acnt, limit):
-    """Create a new credit card instance.
+# def lcs(a, b):
+#     lengths = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
+#     # row 0 and column 0 are initialized to 0 already
+#     print(lengths)
+#     for i, x in enumerate(a):
+#         for j, y in enumerate(b):
+#             if x == y:
+#                 print(x,y)
+#                 lengths[i+1][j+1] = lengths[i][j] + 1
+#             else:
+#                 lengths[i+1][j+1] = max(lengths[i+1][j], lengths[i][j+1])
+#     # read the substring out from the matrix
+#     result = ""
+#     x, y = len(a), len(b)
+#     print(lengths)
+#     while x != 0 and y != 0:
+#         if lengths[x][y] == lengths[x-1][y]:
+#             x -= 1
+#         elif lengths[x][y] == lengths[x][y-1]:
+#             y -= 1
+#         else:
+#             assert a[x-1] == b[y-1]
+#             result = a[x-1] + result
+#             x -= 1
+#             y -= 1
+#     return result
 
-    The initial balance is zero.
+def lcs(A, L):
+    sequence = []
+    for i in range(len(A)):
+        for k in range(len(L)):
+            if A[i]==L[k]:
+                sequence.append(L[k])
+                break
+    return sequence
 
-    customer  the name of the customer (e.g., 'John Bowman')
-    bank      the name of the bank (e.g., 'California Savings')
-    acnt      the acount identifier (e.g., '5391 0375 9387 5309')
-    limit     credit limit (measured in dollars)
-    """
-    self._customer = customer
-    self._bank = bank
-    self._account = acnt
-    self._limit = limit
-    self._balance = 0
+print(lcs('squirrel', 'square'))
 
-  def get_customer(self):
-    """Return name of the customer."""
-    return self._customer
-
-  def get_bank(self):
-    """Return the bank's name."""
-    return self._bank
-
-  def get_account(self):
-    """Return the card identifying number (typically stored as a string)."""
-    return self._account
-
-  def get_limit(self):
-    """Return current credit limit."""
-    return self._limit
-
-  def get_balance(self):
-    """Return current balance."""
-    return self._balance
-
-  def charge(self, price):
-    """Charge given price to the card, assuming sufficient credit limit.
-
-    Return True if charge was processed; False if charge was denied.
-    """
-    if price + self._balance > self._limit:  # if charge would exceed limit,
-      return False                           # cannot accept charge
-    else:
-      self._balance += price
-      return True
-
-  def make_payment(self, amount):
-    """Process customer payment that reduces balance."""
-    self._balance -= amount
-
-if __name__ == '__main__':
-  wallet = []
-  wallet.append(CreditCard('jorius', 'California Savings',
-                           '5391 0375 9387 5309', 2500) )
-  wallet.append(CreditCard('John Bowman', 'California Federal',
-                           '3485 0399 3395 1954', 3500) )
-  wallet.append(CreditCard('pumpkin', 'California Finance',
-                           '5391 0375 9387 5309', 5000) )
-
-  for val in range(1, 20):
-    wallet[0].charge(val)
-    wallet[1].charge(2*val)
-    wallet[2].charge(3*val)
-
-  for c in range(0,3):
-    print('Customer =', wallet[c].get_customer())
-    print('Bank =', wallet[c].get_bank())
-    print('Account =', wallet[c].get_account())
-    print('Limit =', wallet[c].get_limit())
-    print('Balance =', wallet[c].get_balance())
-    while wallet[c].get_balance() > 100:
-      wallet[c].make_payment(100)
-      print('New balance =', wallet[c].get_balance())
-    print()
+# thisisatest
+# testing123testing
